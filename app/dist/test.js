7ee4f7f491afa30bd28bce7d744dd463e6b02c6f
Dima-macbook <dima@virtuman.com>
2017-05-21 14:20:55 +0300

    added test Image creation function

--- app/dist/test.js
+++ app/dist/test.js
@@ -3,5 +3,39 @@
 /**
  * Created by dima on 18/05/2017.
  */
-console.log('test 5');
-console.log('It Finally Works! 2');
+// import Image from ''
+
+var image = new Image();
+
+image.src = 'img/memory.jpg';
+
+function cutImageUp(_ref) {
+  var _ref$numOfCols = _ref.numOfCols,
+      numOfCols = _ref$numOfCols === undefined ? 20 : _ref$numOfCols,
+      _ref$numOfRows = _ref.numOfRows,
+      numOfRows = _ref$numOfRows === undefined ? 20 : _ref$numOfRows;
+
+  var imagePieces = [];
+  var widthOfOnePiece = 50;
+  var heightOfOnePiece = 50;
+
+  for (var x = 0; x < numOfCols; x += 1) {
+    for (var y = 0; y < numOfRows; y += 1) {
+      var canvas = document.createElement('canvas');
+      canvas.width = widthOfOnePiece;
+      canvas.height = heightOfOnePiece;
+      var context = canvas.getContext('2d');
+      context.drawImage(image, x * widthOfOnePiece, y * heightOfOnePiece, widthOfOnePiece, heightOfOnePiece, 0, 0, canvas.width, canvas.height);
+      imagePieces.push(canvas.toDataURL());
+    }
+  }
+  return imagePieces;
+
+  // imagePieces now contains data urls of all the pieces of the image
+
+  // load one piece onto the page
+  var anImageElement = document.getElementById('myImageElementInTheDom');
+  anImageElement.src = imagePieces[0];
+}
+
+image.onload = cutImageUp;
--- src/test.js
+++ src/test.js
@@ -1,5 +1,35 @@
 /**
  * Created by dima on 18/05/2017.
  */
-console.log('test 5');
-console.log('It Finally Works! 2');
+// import Image from ''
+
+const image = new Image();
+
+image.src = 'img/memory.jpg';
+
+function cutImageUp({ numOfCols = 20, numOfRows = 20 }) {
+  const imagePieces = [];
+  const widthOfOnePiece = 50;
+  const heightOfOnePiece = 50;
+
+  for (let x = 0; x < numOfCols; x += 1) {
+    for (let y = 0; y < numOfRows; y += 1) {
+      const canvas = document.createElement('canvas');
+      canvas.width = widthOfOnePiece;
+      canvas.height = heightOfOnePiece;
+      const context = canvas.getContext('2d');
+      context.drawImage(image, x * widthOfOnePiece, y * heightOfOnePiece, widthOfOnePiece, heightOfOnePiece, 0, 0, canvas.width, canvas.height);
+      imagePieces.push(canvas.toDataURL());
+    }
+  }
+  return imagePieces;
+
+  // imagePieces now contains data urls of all the pieces of the image
+
+  // load one piece onto the page
+  const anImageElement = document.getElementById('myImageElementInTheDom');
+  anImageElement.src = imagePieces[0];
+}
+
+image.onload = cutImageUp;
+
