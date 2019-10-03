784b97aa3ec722ede3a8232e583ca23cdc5a811b
Dmitry Kucher <dima@virtuman.com>
2019-10-03 20:48:04 +0300

    button mode expertiment

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@
 import { Application } from '@pixi/app';
 
 // In order that PIXI could render things we need to register appropriate plugins
-import { Renderer, BatchRenderer } from '@pixi/core'; // Renderer is the class that is going to register plugins
+import { Renderer, BatchRenderer, Texture } from '@pixi/core'; // Renderer is the class that is going to register plugins
+
+import { Rectangle } from '@pixi/math';
 
 import { TickerPlugin } from '@pixi/ticker';
 
 // And just for convenience let's register Loader plugin in order to use it
 // right from Application instance like app.loader.add(..) etc.
 import { AppLoaderPlugin } from '@pixi/loaders';
+
 
 // Sprite is our image on the stage
 import { Sprite } from '@pixi/sprite';
@@ -25,9 +28,17 @@ const app = new Application({
 document.body.appendChild(app.view); // Create Canvas tag in the body
 
 // Load the logo
-app.loader.add('logo', './assets/logo.png');
+// app.loader.add('logo', './assets/logo.png');
 app.loader.load(() => {
-  const sprite = Sprite.from('logo');
+  const texture = Texture.from('./assets/logo.png');
+  const sprite = new Sprite(texture);
+
+  // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
+  sprite.interactive = true;
+
+  // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
+  sprite.buttonMode = true;
+
   sprite.anchor.set(0.5); // We want to rotate our sprite relative to the center, so 0.5
   app.stage.addChild(sprite);
 
@@ -36,7 +47,7 @@ app.loader.load(() => {
   sprite.y = app.screen.height * 0.5;
 
   // Put the rotating function into the update loop
-  app.ticker.add((delta) => {
-    sprite.rotation += 0.02 * delta;
-  });
+  // app.ticker.add((delta) => {
+  //   sprite.rotation += 0.02 * delta;
+  // });
 });
